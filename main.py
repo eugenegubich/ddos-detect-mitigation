a70372692e459c99e9c51da34c5a34344d96f657
Eugene Gubich <eugengubich@gmail.com>
2025-01-27 11:29:46 +0000

    dotenv

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ def netplan_disable_ip(conf_path, address):
 
 
 if __name__ == "__main__":
-    dotenv.load_dotenv()
+    dotenv.load_dotenv(find_dotenv())
     if get_conntrack_usage_percent() > int(os.getenv("THRESHOLD")):
         most_connected_ip = most_connected_ip(connect_ip_parse(os.getenv("LOCAL_IPS_SUBNET"), os.getenv("LOCAL_PORT")))
         netplan_disable_ip(os.getenv("NETPLAN_CONF_PATH"), most_connected_ip)
